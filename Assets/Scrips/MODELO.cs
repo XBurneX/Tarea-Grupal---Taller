35f4ce6ded5fa7b9db863e119694134bcecb0c01
Alejandro <killer.jano1470@gmail.com>
2020-05-18 14:48:49 -0500

    MVC actualizacion + Controllador --Alejandro Medina

---
+++ Assets/Scrips/MODELO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MODELO : MonoBehaviour {
+    public static int Score;
+    Text text;
+    public float MoveVelocity;
+    public int LifeToAdd;
+    public int LifepenaltyOnEnter;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+}
---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private float s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<MODELO>().MoveVelocity;
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position = new Vector3(transform.position.x + (h * s), transform.position.y, transform.position.z + (v * s));
+    }
+}
