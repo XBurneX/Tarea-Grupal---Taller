e1d9fc0e90ef804c80835986a7d4d7a60a3c2454
Jafet <andresjeyminez@gmail.com>
2020-05-18 21:45:27 -0500

    Primer commit Vista

---
+++ Assets/Scrips/PlayerVista.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerVista : MonoBehaviour
+{
+    public MODELO model;
+    public Text textovida;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textovida.text = "Vida:" + model.LifeToAdd;
+    }
+}
