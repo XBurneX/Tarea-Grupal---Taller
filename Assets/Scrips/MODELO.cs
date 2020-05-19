790bba1ffb098c80dad2bb23b23238a29b9a7823
sebastianllerenacruz <sebastianllerenacruz@gmail.com>
2020-05-18 22:52:46 -0500

    eliminacion de variable y  un Engine en desuso

--- Assets/Scrips/MODELO.cs
+++ Assets/Scrips/MODELO.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MODELO : MonoBehaviour {
-    Text text;
+   
     public float MoveVelocity;
     public int LifeToAdd;
     public int LifepenaltyOnEnter;
